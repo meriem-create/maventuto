72d0c59db084f838fe3dc027ef45ba5c3fc5df1c
Meriem BenMoussa <benmoussameriem@yahoo.com>
2023-05-05 11:29:10 +0100

    add close command

--- src/main/java/pack1/class1.java
+++ src/main/java/pack1/class1.java
@@ -5,14 +5,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class class1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\benmo\\Desktop\\JARS\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
 		driver.get("https://www.selenium.dev/");
 		 System.out.println("open selenium website");
-		
+		 
+	Thread.sleep(1000); 
+	driver.close();
+	 System.out.println("close selenium website");
+		 
 	} 
 
 }
