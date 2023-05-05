22fc10a5051a30f06407246085cd194547ce68eb
Meriem BenMoussa <benmoussameriem@yahoo.com>
2023-05-05 15:44:50 +0100

    add open selenium webdriver site

--- src/main/java/pack1/class1.java
+++ src/main/java/pack1/class1.java
@@ -1,5 +1,6 @@
 package pack1;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -13,10 +14,13 @@ public class class1 {
 		driver.get("https://www.selenium.dev/");
 		 System.out.println("open selenium website");
 		 
-	Thread.sleep(1000); 
-	driver.close();
-	 System.out.println("close selenium website");
-		 
+	
+	 driver.findElement(By.xpath("//body/div[1]/main[1]/section[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/a[1]")).click();
+	 System.out.println("open selenium webdriver");
+	 Thread.sleep(4000); 
+	 driver.close();
+		 System.out.println("close selenium website");	 
+	 
 	} 
 
 }
